97ff5b05209ee16ec6ff91e99338f842e8520153
kimebmen <simbolonkimebmen@gmail.com>
2020-07-17 19:16:42 +0800

    first commit

---
+++ jobs/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=100)
+    summary = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
